d0d86d883eae18f5f6920ce7d0266fa863e61aed
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-17 23:08:46 -0800

    walleye : ims/rcs support

--- walleye/Android.mk
+++ walleye/Android.mk
@@ -94,6 +94,17 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := ims
+LOCAL_MODULE_OWNER := google
+LOCAL_SRC_FILES := proprietary/app/ims/ims.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := vzw_msdc_api
 LOCAL_MODULE_OWNER := google
 LOCAL_SRC_FILES := proprietary/app/vzw_msdc_api/vzw_msdc_api.apk
