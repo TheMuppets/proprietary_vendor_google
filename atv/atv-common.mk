0092b4e95aebcbeafecd65357eba3694e90a427d
Nolen Johnson <johnsonnolen@gmail.com>
2018-03-17 17:36:11 -0400

    atv: Remove SetupWraith
    * As of 8.0 SetupWraith no longer seems to need to be platform signed, move it to GApps

--- atv/atv-common.mk
+++ atv/atv-common.mk
@@ -13,5 +13,4 @@
 # limitations under the License.
 
 PRODUCT_PACKAGES += LeanbackIme \
-                    LeanbackLauncher \
-                    SetupWraith
+                    LeanbackLauncher
