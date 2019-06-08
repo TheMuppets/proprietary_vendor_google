b944517f3517cedd56c88b6e0042470e12e99235
Rashed Abdel-Tawab <rashed@linux.com>
2019-06-08 12:42:17 -0700

    wahoo: Update blobs from pq3a.190605.003

--- taimen/taimen-vendor.mk
+++ taimen/taimen-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,13 +22,34 @@ PRODUCT_COPY_FILES += \
     vendor/google/taimen/proprietary/etc/firmware/dnd.sound_model:system/etc/firmware/dnd.sound_model \
     vendor/google/taimen/proprietary/etc/firmware/music_detector.descriptor:system/etc/firmware/music_detector.descriptor \
     vendor/google/taimen/proprietary/etc/firmware/music_detector.sound_model:system/etc/firmware/music_detector.sound_model \
+    vendor/google/taimen/proprietary/etc/permissions/com.android.omadm.service.xml:system/etc/permissions/com.android.omadm.service.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.android.sdm.plugins.connmo.xml:system/etc/permissions/com.android.sdm.plugins.connmo.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.android.sdm.plugins.sprintdm.xml:system/etc/permissions/com.android.sdm.plugins.sprintdm.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.android.vzwomatrigger.xml:system/etc/permissions/com.android.vzwomatrigger.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.verizon.apn.xml:system/etc/permissions/com.verizon.apn.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.verizon.embms.xml:system/etc/permissions/com.verizon.embms.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.verizon.llkagent.xml:system/etc/permissions/com.verizon.llkagent.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.verizon.provider.xml:system/etc/permissions/com.verizon.provider.xml \
+    vendor/google/taimen/proprietary/etc/permissions/com.verizon.services.xml:system/etc/permissions/com.verizon.services.xml \
+    vendor/google/taimen/proprietary/etc/permissions/features-verizon.xml:system/etc/permissions/features-verizon.xml \
+    vendor/google/taimen/proprietary/etc/permissions/obdm_permissions.xml:system/etc/permissions/obdm_permissions.xml \
     vendor/google/taimen/proprietary/etc/permissions/privapp-permissions-wahoo.xml:system/etc/permissions/privapp-permissions-wahoo.xml \
+    vendor/google/taimen/proprietary/etc/permissions/vzw_mvs_permissions.xml:system/etc/permissions/vzw_mvs_permissions.xml \
+    vendor/google/taimen/proprietary/etc/sysconfig/whitelist_com.android.omadm.service.xml:system/etc/sysconfig/whitelist_com.android.omadm.service.xml \
+    vendor/google/taimen/proprietary/framework/PowerAnomalyDataModemInterface.jar:system/framework/PowerAnomalyDataModemInterface.jar \
+    vendor/google/taimen/proprietary/framework/PowerAnomalyQcril.jar:system/framework/PowerAnomalyQcril.jar \
+    vendor/google/taimen/proprietary/framework/VerizonUnifiedSettings.jar:system/framework/VerizonUnifiedSettings.jar \
+    vendor/google/taimen/proprietary/framework/vendor.qti.qcril.am-V1.0-java.jar:system/framework/vendor.qti.qcril.am-V1.0-java.jar \
     vendor/google/taimen/proprietary/lib/com.qualcomm.qti.imsrtpservice@1.0.so:system/lib/com.qualcomm.qti.imsrtpservice@1.0.so \
     vendor/google/taimen/proprietary/lib/lib-imsvideocodec.so:system/lib/lib-imsvideocodec.so \
     vendor/google/taimen/proprietary/lib/lib-imsvt.so:system/lib/lib-imsvt.so \
     vendor/google/taimen/proprietary/lib/lib-imsvtextutils.so:system/lib/lib-imsvtextutils.so \
     vendor/google/taimen/proprietary/lib/lib-imsvtutils.so:system/lib/lib-imsvtutils.so \
     vendor/google/taimen/proprietary/lib/libdiag_system.so:system/lib/libdiag_system.so \
+    vendor/google/taimen/proprietary/lib/libdmengine.so:system/lib/libdmengine.so \
+    vendor/google/taimen/proprietary/lib/libdmjavaplugin.so:system/lib/libdmjavaplugin.so \
+    vendor/google/taimen/proprietary/lib/libeaselcomm.so:system/lib/libeaselcomm.so \
+    vendor/google/taimen/proprietary/lib/libeaselcontrol.amber.so:system/lib/libeaselcontrol.amber.so \
     vendor/google/taimen/proprietary/lib/libimscamera_jni.so:system/lib/libimscamera_jni.so \
     vendor/google/taimen/proprietary/lib/libimsmedia_jni.so:system/lib/libimsmedia_jni.so \
     vendor/google/taimen/proprietary/lib/librcc.so:system/lib/librcc.so \
@@ -37,12 +58,41 @@ PRODUCT_COPY_FILES += \
     vendor/google/taimen/proprietary/lib64/lib-imsvt.so:system/lib64/lib-imsvt.so \
     vendor/google/taimen/proprietary/lib64/lib-imsvtextutils.so:system/lib64/lib-imsvtextutils.so \
     vendor/google/taimen/proprietary/lib64/lib-imsvtutils.so:system/lib64/lib-imsvtutils.so \
+    vendor/google/taimen/proprietary/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so \
+    vendor/google/taimen/proprietary/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so \
     vendor/google/taimen/proprietary/lib64/libdiag_system.so:system/lib64/libdiag_system.so \
+    vendor/google/taimen/proprietary/lib64/libeaselcomm.so:system/lib64/libeaselcomm.so \
+    vendor/google/taimen/proprietary/lib64/libeaselcontrol.amber.so:system/lib64/libeaselcontrol.amber.so \
     vendor/google/taimen/proprietary/lib64/libimscamera_jni.so:system/lib64/libimscamera_jni.so \
     vendor/google/taimen/proprietary/lib64/libimsmedia_jni.so:system/lib64/libimsmedia_jni.so \
-    vendor/google/taimen/proprietary/lib64/librcc.so:system/lib64/librcc.so
+    vendor/google/taimen/proprietary/lib64/librcc.so:system/lib64/librcc.so \
+    vendor/google/taimen/proprietary/priv-app/EuiccGoogle/esim0.img:system/priv-app/EuiccGoogle/esim0.img
 
 PRODUCT_PACKAGES += \
+    EaselServicePrebuilt \
+    Tycho \
+    VZWAPNLib \
+    embms \
+    ims \
+    uceShimService \
+    vzw_msdc_api \
+    CNEService \
+    CarrierSetup \
+    ConnMO \
+    DCMO \
+    DMService \
+    DiagMon \
+    EuiccGoogle \
+    HotwordEnrollmentOKGoogleWCD9340 \
+    HotwordEnrollmentTGoogleWCD9340 \
+    HotwordEnrollmentXGoogleWCD9340 \
+    OBDM_Permissions \
+    OemDmTrigger \
+    SprintDM \
+    SprintHM \
+    VzwOmaTrigger \
+    obdm_stub \
+    qcrilmsgtunnel \
     cneapiclient \
     com.quicinc.cne.api-V1.0-java \
     com.quicinc.cne \
