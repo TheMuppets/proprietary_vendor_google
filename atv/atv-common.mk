f221f31d46a4af0e42d37308a2839ab5183e779c
Aaron Kling <webgeek1234@gmail.com>
2019-06-29 12:09:22 -0500

    ATV: Update launcher
    The normal atv launcher cannot be used due to gms dependencies.
    This non-gms version was released with the yukawa on googlesource.

--- atv/atv-common.mk
+++ atv/atv-common.mk
@@ -13,4 +13,5 @@
 # limitations under the License.
 
 PRODUCT_PACKAGES += LeanbackIme \
-                    LeanbackLauncher
+                    TVLauncher \
+                    TVRecommendations
