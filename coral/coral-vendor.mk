5ef8a6ea14949afaf3158a5459f3c04eecc2949d
razorloves <razorloves@gmail.com>
2020-08-08 15:28:31 -0500

    floral: Add new codec2 proprietary flies
    Google removed the codec2.vendor.ext.policy file from the device repo
    so it needs to be pulled from the factory image now.
    The codec2.vendor.base.policy file is a new blob added in Aug 2020
    factory image, which is needed for vendor.qti.media.c2@1.0-service.
    
    Change-Id: I6a8cdbf3677b31917ad6aa6028ebb2b22d3241ec

--- coral/coral-vendor.mk
+++ coral/coral-vendor.mk
@@ -697,6 +697,8 @@ PRODUCT_COPY_FILES += \
     vendor/google/coral/proprietary/vendor/etc/qdcm_calib_data_nt37280_amoled_5.68_cmd_mode_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_nt37280_amoled_5.68_cmd_mode_panel.xml \
     vendor/google/coral/proprietary/vendor/etc/qdcm_calib_data_s6e3hc2_amoled_6.2_cmd_mode_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_s6e3hc2_amoled_6.2_cmd_mode_panel.xml \
     vendor/google/coral/proprietary/vendor/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    vendor/google/coral/proprietary/vendor/etc/seccomp_policy/codec2.vendor.base.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.base.policy \
+    vendor/google/coral/proprietary/vendor/etc/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     vendor/google/coral/proprietary/vendor/etc/sensors/config/ak991x_polling_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/ak991x_polling_0.json \
     vendor/google/coral/proprietary/vendor/etc/sensors/config/bmp380.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/bmp380.json \
     vendor/google/coral/proprietary/vendor/etc/sensors/config/c2_bmp380.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/c2_bmp380.json \
