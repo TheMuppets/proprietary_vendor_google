3b459d4828675afabb1fc55af0a844f1afe3162a
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-10 14:48:40 -0500

    Import wahoo family from `lineage-18.1`

---
+++ walleye/proprietary/vendor/radio/qcril_database/upgrade/10_israel_normal_ecc.sql
@@ -0,0 +1,15 @@
+/*
+New data-base items added by Google
+10. 101 for ambulance and 102 for fire should be dialed thru normal routing in Israel.
+*/
+
+BEGIN TRANSACTION;
+
+INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 10);
+
+INSERT OR REPLACE INTO qcril_emergency_source_mcc_table (MCC, NUMBER, IMS_ADDRESS, SERVICE) VALUES ('425','101','','');
+INSERT OR REPLACE INTO qcril_emergency_source_mcc_table (MCC, NUMBER, IMS_ADDRESS, SERVICE) VALUES ('425','102','','');
+INSERT OR REPLACE INTO qcril_emergency_source_voice_table (MCC, NUMBER, IMS_ADDRESS, SERVICE) VALUES ('425','101','','full');
+INSERT OR REPLACE INTO qcril_emergency_source_voice_table (MCC, NUMBER, IMS_ADDRESS, SERVICE) VALUES ('425','102','','full');
+
+COMMIT TRANSACTION;
